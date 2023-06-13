e945f2680e4fa3cf06b500bcec64cc27751281c7
“hashem_muhammed” <hashemuhammed@gmail.com>
2023-06-13 22:14:46 +0300

    added the pillow package to the requirments

--- products/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=150)
-    image = models.ImageField(null=True, blank=True)
-    quantity = models.PositiveIntegerField(null= False)
-    description = models.TextField(null=True , blank=True)
-    updated_at = models.DateTimeField(auto_now= True, blank=True)
--- wish_list/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from products.models import Product 
-from users.models import UserModel
-# Create your models here.
-class WishList(models.Model):
-    products = models.ManyToManyField(Product) 
-    user = models.OneToOneField(UserModel , related_name = 'wishlist' , on_delete=models.CASCADE)
